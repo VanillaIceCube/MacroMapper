a207cf5f676302f78dab3284fbaa68037ab79305
IceCobe <jaalaba@gmail.com>
2026-08-30 09:28:46 -0700

    test: adapt component tests to MUI v9

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -6,7 +6,9 @@ vi.mock('@mui/material', async () => {
   return {
     ...actual,
     Menu: ({ open, children }) =>
-      open ? React.createElement('div', { 'data-testid': 'menu' }, children) : null,
+      open
+        ? React.createElement(actual.MenuList, { 'data-testid': 'menu' }, children)
+        : null,
     TextField: ({ inputRef, InputProps, inputProps, ...props }) =>
       React.createElement(actual.TextField, {
         ...props,
